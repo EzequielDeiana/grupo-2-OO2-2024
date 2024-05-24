9f967f07eb11a14845cf3c25368493ebd7b35ad8
Tomás Diaz De Maio <111838813+TomasDiazDeMaio@users.noreply.github.com>
2024-05-24 17:57:28 -0300

    ProductoVendidoController
    Funciones

--- grupo-2/src/main/java/com/unla/oo2/grupo2/controller/ProductoVendidoController.java
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/controller/ProductoVendidoController.java
@@ -1,5 +1,22 @@
 package com.unla.oo2.grupo2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.oo2.grupo2.helper.RouteHelper;
+
 public class ProductoVendidoController {
+	@GetMapping("/index")
+	public ModelAndView index() {
+		return new ModelAndView(RouteHelper.HOME_INDEX);
+	}
+
+	@GetMapping("/")
+	public RedirectView redirectHome() {
+		return new RedirectView(RouteHelper.INDEX);
+	}
 
 }
+
+
