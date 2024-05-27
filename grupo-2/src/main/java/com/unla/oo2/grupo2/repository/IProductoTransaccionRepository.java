cc51d95cc436bfd9258762e4584df76e7401255a
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-05-27 13:25:08 -0300

    adding repository for Entities - in progress

---
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/repository/IProductoTransaccionRepository.java
@@ -0,0 +1,11 @@
+package com.unla.oo2.grupo2.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.oo2.grupo2.entity.Producto;
+
+public interface IProductoTransaccionRepository extends JpaRepository<IProductoTransaccionRepository, Serializable> {
+
+}
---
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/repository/IProductoVendidoRepository.java
@@ -0,0 +1,12 @@
+package com.unla.oo2.grupo2.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unla.oo2.grupo2.entity.Producto;
+import com.unla.oo2.grupo2.entity.ProductoVendido;
+
+public interface IProductoVendidoRepository extends JpaRepository<ProductoVendido, Serializable> {
+
+}
