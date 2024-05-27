6585751cb9fdda10e7073710bdac38420e9db987
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-05-27 15:38:13 -0300

    add CRUD for Admin Entity

---
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/service/AdminService.java
@@ -0,0 +1,39 @@
+package com.unla.oo2.grupo2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.unla.oo2.grupo2.entity.Admin;
+import com.unla.oo2.grupo2.repository.IAdminRepository;
+
+@Service
+public class AdminService {
+
+	@Autowired
+	IAdminRepository adminRepo;
+
+	// cRud
+	public Admin getById(int id) {
+		return adminRepo.getReferenceById(id);
+
+	}
+
+	public List<Admin> getAll() {
+		return adminRepo.findAll();
+	}
+
+	// Crud
+	public int create(Admin admin)
+	{
+		return adminRepo.save(admin).getId();
+	}
+	
+	public void delete(int id)
+	{
+		if(id != 0)
+			adminRepo.deleteById(id);
+	}
+	
+}
