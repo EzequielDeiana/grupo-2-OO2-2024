cf86574801da9e74ee6c4b3c3beabc8e0499b8a0
Ezequiel Agustin González <ezequielgonzalezdei@gmail.com>
2024-06-02 00:14:02 -0300

    Error Controller fixed

--- grupo-2/src/main/java/com/unla/oo2/grupo2/controller/CustomErrorController.java
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/controller/CustomErrorController.java
@@ -1,5 +1,6 @@
 package com.unla.oo2.grupo2.controller;
 
+import java.io.Console;
 import java.util.Map;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -11,18 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping()
 public class CustomErrorController implements ErrorController {
-
 	@GetMapping("/error")
 	public ModelAndView handleError(HttpServletRequest request, Map<String, Object> model) {
 		ModelAndView modelAndView = new ModelAndView("/error/error");
-
 		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
 		String errorMessage = (String) request.getAttribute("javax.servlet.error.message");
+		
 
-		model.put("statusCode", statusCode);
-		model.put("errorMessage", errorMessage);
 
 		modelAndView.addObject("statusCode", statusCode);
 		modelAndView.addObject("errorMessage", errorMessage);
---
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.unla.oo2.grupo2.controller;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ModelAndView handleHttpRequestMethodNotSupported(HttpServletRequest request, ServletResponse res, HttpRequestMethodNotSupportedException ex) {
+        ModelAndView modelAndView = new ModelAndView("/error/error");
+        int status = ((HttpServletResponse) res).getStatus();
+        System.out.println((Integer) request.getAttribute("javax.servlet.error.status_code"));
+        
+        modelAndView.addObject("statusCode", status);
+        modelAndView.addObject("errorMessage", ex.getMessage());
+        return modelAndView;
+    }
+}
