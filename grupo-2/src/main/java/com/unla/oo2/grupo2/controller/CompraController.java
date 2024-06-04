76e2071e92778543c3d70fa821ce1faf738c06bf
Ezequiel Agustin González <ezequielgonzalezdei@gmail.com>
2024-06-04 03:03:09 -0300

    Compra (index/update/new) added

--- grupo-2/src/main/java/com/unla/oo2/grupo2/controller/CompraController.java
+++ grupo-2/src/main/java/com/unla/oo2/grupo2/controller/CompraController.java
@@ -10,32 +10,43 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.unla.oo2.grupo.serviceInterfaces.ICompraServices;
+import com.unla.oo2.grupo.serviceInterfaces.IPedidoCompra;
+import com.unla.oo2.grupo.serviceInterfaces.IProducto;
 import com.unla.oo2.grupo2.entity.Compra;
+import com.unla.oo2.grupo2.service.PedidoCompraService;
 
 @Controller
 @RequestMapping("/compra")
 public class CompraController {
 
 	private ICompraServices compraService;
+	private PedidoCompraService pedidoCompra;
+	private IProducto productoService;
 
-	public CompraController(ICompraServices compraService) {
+	public CompraController(ICompraServices compraService, PedidoCompraService pedidoCompra, IProducto productoService) {
 		this.compraService = compraService;
+		this.pedidoCompra = pedidoCompra;
+		this.productoService = productoService;
 	}
 
 	@GetMapping("/index")
 	public ModelAndView index() {
-		return new ModelAndView("compra/index").addObject("compra", compraService.getAll());
+		ModelAndView modelAndView = new ModelAndView("compra/index");
+				modelAndView.addObject("compras", compraService.getAll());
+			
+			return modelAndView;
 	}
 
 	@GetMapping("/")
 	public RedirectView redirectToHomeIndex() {
-		return new RedirectView("/person/index");
+		return new RedirectView("/index");
 	}
 
 	@GetMapping("/new")
 	public ModelAndView createForm() {
 		ModelAndView model = new ModelAndView("/compra/new");
-		model.addObject("person", new Compra());
+		model.addObject("pedidosDeCompra", pedidoCompra.getAll());
+		model.addObject("compra", new Compra());
 		return model;
 	}
 
@@ -46,19 +57,20 @@ public class CompraController {
 	}
 
 	@GetMapping("/{id}")
-	public ModelAndView get(@PathVariable("id") Long id) throws Exception {
+	public ModelAndView get(@PathVariable("id") int id) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("/compra/update");
 		modelAndView.addObject("compra", compraService.findById(id).get());
 		return modelAndView;
 	}
-
+/*
 	@GetMapping("/{name}")
 	public ModelAndView get(@PathVariable("name") String name) throws Exception {
 		ModelAndView modelAndView = new ModelAndView("/compra/update");
 		modelAndView.addObject("compra", compraService.findByName(name).get());
 		return modelAndView;
 	}
-
+	*/
+	
 	@PostMapping("/{id}")
 	public RedirectView update(@ModelAttribute("compra") Compra compra) {
 		compraService.insertOrUpdate(compra);
